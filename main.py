404787aeffacd266ff7a890331ceafd42267af46
LCF2370 <c0921675@mylambton.ca>
2024-10-29 17:30:42 -0400

    Modified main.py to import Flask. Also create routes for every templates. Error: unable to run the web page.

--- main.py
+++ main.py
@@ -1,11 +1,32 @@
-from flask import Flask
+# Imports the Flask Web Framework
+from flask import Flask, render_template
+# Creates the application instance
+app = Flask(__name__)
 
 app = Flask(__name__)
 
-@app.route('/')
+# Routing
+@app.route("/")
+@app.route("/home")
 def index():
-    return 'Hello World'
+    return render_template("home.html")
 
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route("/projects")
+def projects():
+    return render_template("projects.html")
+
+@app.route("/services")
+def services():
+    return render_template("services.html")
+
+@app.route("/contact")
+def contacts():
+    return render_template("contact.html")
+
+# Creates a hosting IP address to run the website
 if __name__ == '__main__':
-    app.run()
-
+   app.run()
